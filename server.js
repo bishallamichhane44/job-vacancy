3b889579f32be51184a874064f991b385f642b63
roshankc00 <120471898+roshankc00@users.noreply.github.com>
2024-06-12 03:03:21 +0545

    feat:added the dockerfiles and nginx proxy

--- server.js
+++ server.js
@@ -29,14 +29,19 @@ app.use("/api/jobs", require("./Routes/jobPosts"));
 app.use("/api/auth", require("./Routes/auth"));
 
 //Serve static assets in production
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"));
+// if (process.env.NODE_ENV === "production") {
+//   // Set static folder
+//   app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
+
+app.get("/api/he", (req, res) => {
+  res.send("Hello Roshan");
+  console.log("hahahaha");
+});
 const connectandStart = async () => {
   try {
     await mongoose.connect(MONGO_URI, {
