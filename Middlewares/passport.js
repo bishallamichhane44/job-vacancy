3d8c96dc040ad8c4d181a63f0bc57e77384b2d19
roshankc00 <120471898+roshankc00@users.noreply.github.com>
2024-06-12 01:32:48 +0545

    CHORE: initial commit

--- Middlewares/passport.js
+++ Middlewares/passport.js
@@ -1,48 +1,47 @@
-const User = require('../Models/User');
-const config = require('config');
-const SECRET = config.get('APP_SECRET');
-const { Strategy, ExtractJwt } = require('passport-jwt');
-const { userAuth } = require('../Middlewares/userAuth');
+const User = require("../Models/User");
+const { Strategy, ExtractJwt } = require("passport-jwt");
+const { userAuth } = require("../Middlewares/userAuth");
+const { JWT_SECRET } = require("../config");
 
 const opts = {
-	jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-	secretOrKey: SECRET,
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: JWT_SECRET,
 };
 
 const jwtPassport = (passport) => {
-	passport.use(
-		new Strategy(opts, async (payload, done) => {
-			try {
-				const user = await User.findById(payload.user_id);
-				if (user) {
-					//we can implement our logger function here to keep track
-					//of every logs using pakages like morgan or winston
+  passport.use(
+    new Strategy(opts, async (payload, done) => {
+      try {
+        const user = await User.findById(payload.user_id);
+        if (user) {
+          //we can implement our logger function here to keep track
+          //of every logs using pakages like morgan or winston
 
-					return done(null, user); //appends the user to the req obj
-				}
-				//logger function
-				return done(null, false);
-			} catch (err) {
-				return done(null, err);
-			}
-		})
-	);
+          return done(null, user); //appends the user to the req obj
+        }
+        //logger function
+        return done(null, false);
+      } catch (err) {
+        return done(null, err);
+      }
+    })
+  );
 };
 
 const serializeUser = (user) => {
-	return {
-		role: user.role,
-		username: user.username,
-		email: user.email,
-		name: user.name,
-		_id: user._id,
-		avatar: user.avatar,
-		updatedAt: user.updatedAt,
-		createdAt: user.createdAt,
-	};
+  return {
+    role: user.role,
+    username: user.username,
+    email: user.email,
+    name: user.name,
+    _id: user._id,
+    avatar: user.avatar,
+    updatedAt: user.updatedAt,
+    createdAt: user.createdAt,
+  };
 };
 
 module.exports = {
-	jwtPassport,
-	serializeUser,
+  jwtPassport,
+  serializeUser,
 };
--- Models/AdminCompany.js
+++ Models/AdminCompany.js
@@ -1,15 +1,15 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 const companySchema = new Schema({
-	name: {
-		type: String,
-	},
-	noOfJobs: {
-		type: Number,
-		default: 0,
-	},
+  name: {
+    type: String,
+  },
+  noOfJobs: {
+    type: Number,
+    default: 0,
+  },
 });
 
-const Company = model('companies', companySchema);
+const Company = model("companies", companySchema);
 
 module.exports = Company;
